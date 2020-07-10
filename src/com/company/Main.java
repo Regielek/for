0773149e5a7cb17d9207af770cf08b049a91c42b
unknown <ferlmick857@gmail.com>
2020-07-10 09:29:12 +0200

    poprawka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,17 @@ public class Main
 
     public static void main(String[] args)
     {
-        //int x, y;
+        int x, y;
 
-        //Scanner scanner = new Scanner(System.in);
-        //System.out.print("Podaj liczbę x: ");
-        //x = scanner.nextInt();
-        //System.out.print("Podaj liczbę y: ");
-        //y = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę x: ");
+        x = scanner.nextInt();
+        System.out.print("Podaj liczbę y: ");
+        y = scanner.nextInt();
 
-        for (int z = 0; z <= 10; z++)
+        for (x; x >= y; x++)
         {
-            System.out.println(z);
+            System.out.println(x);
         }
     }
 }
